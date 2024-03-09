f468badb65cc2493c82db063ee28ac20618270c3
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2024-03-09 13:12:18 +0500

    update to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,53 @@
 #!/bin/bash
+echo -e '[1]Full Install | [2]Install deps | [3]Remove'
+read choice
 
-packages='curl stow wl-clipboard neovim tmux htop tldr man-db zoxide fzf ncdu'
+packages='stow wl-clipboard neovim tmux htop tldr man-db zoxide fzf ncdu'
+minimalPackages='stow neovim'
 
-if [[ -f /etc/arch-release ]]; then
-	pacman -Sy --needed $packages github-cli
-elif [[ -f /etc/debian_version ]]; then
-	apt-get update
-	apt-get install $packages gh
-else
-	echo 'Unsuported system'
-fi
+fullInstall() {
+	if [[ -f /etc/arch-release ]]; then
+		pacman -Sy --needed $packages github-cli
+	elif [[ -f /etc/debian_version ]]; then
+		apt-get update
+		apt-get install $packages gh
+	else
+		echo 'Unsuported system'
+	fi
+}
+
+install() {
+	if [[ -f /etc/arch-release ]]; then
+		pacman -Sy --needed $minimalPackages
+	elif [[ -f /etc/debian_version ]]; then
+		apt-get update
+		apt-get install $minimalPackages
+	else
+		echo 'Unsuported system'
+	fi
+}
+
+remove() {
+	if [[ -f /etc/arch-release ]]; then
+		pacman -Rns $packages github-cli
+	elif [[ -f /etc/debian_version ]]; then
+		apt-get purge $packages gh
+	else
+		echo 'Unsuported system'
+	fi
+}
+
+case $choice in
+	"1")
+		fullInstall
+		;;
+	"2")
+		install
+		;;
+	"3")
+		remove
+		;;
+	*)
+		echo "Incorrect input"
+		;;
+esac
