43d64230c1bf0c3a9419aae53f326bb72a965763
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2024-03-12 11:40:04 +0500

    script updates

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-yay && echo -e "Pacman/aur updated\n" && flatpak update && plasma-discover-update 2>/dev/null && echo -e "\nDiscover packages updated!" && echo All packages updated > /dev/pts/0
+yay && echo -e "Pacman/aur updated\n"; flatpak update
+plasma-discover-update 2>/dev/null && echo -e "\nDiscover packages updated!"
+echo Packages updated > /dev/pts/0
