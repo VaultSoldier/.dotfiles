615591b38d7f057700371c6d7d0e2425be3aa7e3
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2024-06-19 04:50:51 +0500

    kitty terminal updates and nvim

---
+++ .config/nvim/lua/custom/settings.lua
@@ -0,0 +1,47 @@
+-- NOTE: For more options, you can see `:help option-list`
+
+vim.opt.showmode = true
+vim.opt.signcolumn = "yes"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+--  See `:help 'list' and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Set tab width to 2 spaces
+vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+
+--  See `:help 'clipboard'`
+vim.opt.clipboard = "unnamedplus"
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
