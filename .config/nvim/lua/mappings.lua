4b6646396efb404a0612d4fd0563ab4f20eddea4
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2024-07-29 17:02:29 +0500

    nvim

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,8 +1,8 @@
 require "nvchad.mappings"
 
 -- add yours here
-
 local map = vim.keymap.set
+local builtin = require("telescope.builtin")
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
@@ -28,11 +28,9 @@ map("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic m
 map("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 
 map("n", "<leader>C", "<cmd>Telescope themes<CR>", { desc = "[C]hange Theme" })
-
 map("n", "<leader>ct", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
 map("n", "<leader>sm", "<cmd>Telescope marks<CR>", { desc = "[S]earch [M]arks" })
-map("n", "<leader>sb", "<cmd>Telescope buffers<CR>", { desc = "[S]earch [B]uffers}" })
-
+map("n", "<leader>sb", "<cmd>Telescope buffers<CR>", { desc = "[S]earch [B]uffers" })
 map("n", "<leader>tt", "<cmd>Telescope terms<CR>", { desc = "[T]erminal [T]erminals" })
 
 map("n", "<leader>th", function()
@@ -45,11 +43,45 @@ end, { desc = "[T]erminal [V]ertical" })
 
 map("n", "<leader>wh", "<cmd>NvCheatsheet<CR>", { desc = "Toggle nvcheatsheet" }) -- toggle cheatsheet
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- See `:help telescope.builtin`
+map("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+map("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+map("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+map("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+map("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+map("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch [G]rep" })
+map("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+map("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+map("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+map("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
+-- Slightly advanced example of overriding default behavior and theme
+map("n", "<leader>/", function()
+  -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+  builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+    winblend = 10,
+    previewer = false,
+  }))
+end, { desc = "[/] Fuzzily search in current buffer" })
+
+-- It's also possible to pass additional configuration options.
+--  See `:help telescope.builtin.live_grep()` for information about particular keys
+map("n", "<leader>s/", function()
+  builtin.live_grep({
+    grep_open_files = true,
+    prompt_title = "Live Grep in Open Files",
+  })
+end, { desc = "[S]earch [/] in Open Files" })
+
+-- Shortcut for searching your Neovim configuration files
+map("n", "<leader>sn", function()
+  builtin.find_files({ cwd = vim.fn.stdpath("config") })
+end, { desc = "[S]earch [N]eovim files" })
+
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 -- Disable mappinge
 local nomap = vim.keymap.del
-
 nomap("n", "<leader>ff") -- telescope find files 
 nomap("n", "<leader>fw") -- search by grep
 nomap("n", "<leader>fo") -- find oldfiles
