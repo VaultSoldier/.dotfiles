de5aa7e1b5f6faaa72f8de3f755dfaf8c2fe598c
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2024-09-13 22:49:00 +0500

    update script notification change

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,4 +2,5 @@
 
 yay && echo -e "Pacman/aur updated\n"; flatpak update
 plasma-discover-update 2>/dev/null && echo -e "\nDiscover packages updated!"
-echo Packages updated > /dev/pts/1
+
+notify-send "System Updated\!" --icon=data-success --app-name="Updates"
