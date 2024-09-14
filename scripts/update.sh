41b476964724e5380831218c736155e8ca57c24a
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2024-09-15 02:23:30 +0500

    gaming

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,4 +3,4 @@
 yay && echo -e "Pacman/aur updated\n"; flatpak update
 plasma-discover-update 2>/dev/null && echo -e "\nDiscover packages updated!"
 
-notify-send "System Updated\!" --icon=data-success --app-name="Updates"
+notify-send "System Updated!" --icon=data-success --app-name="Updates"
