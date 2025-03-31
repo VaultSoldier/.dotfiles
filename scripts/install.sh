4c8d2709b7270d1682edcb9b16ec80a0df2e071f
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-03-31 15:10:13 +0500

    YAY installation/deps

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,43 +4,49 @@ while
 	[[ -z $choice || $choice != [1-4] ]] && echo "Incorrect input"
 do true; done
 
-packages='stow wl-clipboard neovim tmux htop btop tealdeer man-db zoxide fzf ncdu ripgrep kitty flatpak'
-minimalPackages='stow neovim ripgrep fzf'
-arch_pkgs='github-cli git-zsh-completion'
+packages='stow wl-clipboard neovim tmux htop btop tealdeer man-db zoxide fzf ncdu ripgrep kitty flatpak gnupg pass'
+minimalPackages='stow neovim ripgrep fzf git'
+arch_pkgs='github-cli git-zsh-completion pass man-db podman podman-desktop podman-docker podman-compose'
 debian_pkgs='gh'
 
-fullInstall() {
+full_install() {
 	while 
 		read -p "Install tailscale? [Y/n]: " tailscaleInstall
 		[[ -z $tailscaleInstall || ! $tailscaleInstall =~ ^[yYnN]$ ]]  
 	do true; done
-	[[ "$tailscaleInstall" == "Y" || "$tailscaleInstall" == "y" ]] && curl -fsSL https://tailscale.com/install.sh | sh
-	[ -f /etc/arch-release ] && pacman -Sy --needed $packages $arch_pkgs
-	[ -f /etc/debian_version ] && apt-get update && apt-get install $packages $debian_pkgs && 
-    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-	systemSupport
+	[[ "$tailscaleInstall" == "Y" || "$tailscaleInstall" == "y" ]] && curl -fsSL https://tailscale.com/install.sh | sh &&
+	[ -f /etc/debian_version ] && flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+	if [ -f /etc/arch-release ]; then
+		source ./yay_install_script.sh
+		sudo pacman -Syu --needed $packages $arch_pkgs && yay_install 
+		rm -rf /tmp/yay-install-*
+	fi
+	
+	[ -f /etc/debian_version ] && sudo apt-get update && apt-get install $packages $debian_pkgs
+	system_support
 }
 
 install() {
-	[ -f /etc/arch-release ] && pacman -Sy --needed $minimalPackages
-	[ -f /etc/debian_version ] && apt-get update && apt-get install $minimalPackages
+	[ -f /etc/arch-release ] && sudo pacman -Syu --needed $minimalPackages
+	[ -f /etc/debian_version ] && sudo apt-get update && apt-get install $minimalPackages
 	systemSupport
 }
 
 remove() {
-	[ -f /etc/arch-release ] && pacman -Rns $packages $arch_pkgs
-	[ -f /etc/debian_version ] && apt-get purge $packages $debian_pkgs && apt-get autoremove
+	[ -f /etc/arch-release ] && sudo pacman -Rns $packages $arch_pkgs
+	[ -f /etc/debian_version ] && sudo apt-get purge $packages $debian_pkgs && apt-get autoremove
 	systemSupport
 }
 
-systemSupport() {
+system_support() {
 	[ ! -f /etc/arch-release ] && [ ! -f /etc/debian_version ] && echo 'Unsuported system'
 }
 
 
 case $choice in
 	"1")
-		fullInstall
+		full_install
 		;;
 	"2")
 		install
