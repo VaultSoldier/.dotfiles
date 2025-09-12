61d0964f6bd1d4b91f99acb2fcacdf8f303a0dec
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-09-13 01:13:19 +0500

    Caelestia module

---
+++ .config/quickshell/shell.nix
@@ -0,0 +1,31 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    qt6.qtbase
+    qt6.qtdeclarative
+    qt6.qtmultimedia
+    qt6.qtsvg
+    kdePackages.kirigami
+    kdePackages.breeze
+    libqalculate
+    pipewire
+    aubio
+    libcava
+    fftw
+    cmake
+    ninja
+    pkg-config
+  ];
+
+  # cmake ./caelestia -B ./caelestia/build -G Ninja \
+  #   -DCMAKE_BUILD_TYPE=Release \
+  #   -DCMAKE_INSTALL_PREFIX=$HOME/.config/quickshell/caelestia \
+  #   -DINSTALL_QMLDIR=$HOME/.config/quickshell/caelestia/qml \
+  #   -DINSTALL_LIBDIR=$HOME/.config/quickshell/caelestia/lib \
+  #   -DINSTALL_QSCONFDIR=$HOME/.config/quickshell/caelestia
+  #
+  # cmake --build ./caelestia/build
+  # cmake --install ./caelestia/build
+}
+
