574672ffdc6c822f1db524a09f6f50eeffcd6978
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-09-19 03:35:13 +0500

    Plasma6 packages

--- Nix/modules/packages/plasma6.nix
+++ Nix/modules/packages/plasma6.nix
@@ -1,5 +1,18 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   services = { desktopManager.plasma6.enable = true; };
+
+  environment.plasma6.excludePackages = with pkgs;
+    [
+      kdePackages.elisa # Simple music player aiming to provide a nice experience for its users
+    ];
+
+  environment.systemPackages = with pkgs; [
+    kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
+    kdePackages.kcolorchooser # A small utility to select a color
+    kdePackages.kolourpaint # Easy-to-use paint program
+    kdePackages.ksystemlog # KDE SystemLog Application
+    wl-clipboard # Command-line copy/paste utilities for Wayland
+  ];
 }
