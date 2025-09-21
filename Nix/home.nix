43ba8cf573648efeeecee6d0fbc1e515196be768
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-09-22 02:41:16 +0500

    Git signature update

--- Nix/home.nix
+++ Nix/home.nix
@@ -172,6 +172,7 @@
     extraConfig = {
       commit.gpgsign = true;
       gpg.format = "ssh";
+      gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
       user.signingkey = "~/.ssh/ssh_keys/git_id_ed25519";
       pull.rebase = true;
     };
