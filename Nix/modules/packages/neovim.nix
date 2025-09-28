d24c8cf9c87a79696b8daa629e1e8d7677cc5f75
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-09-28 05:58:26 +0500

    Neovim: update deps & formatters fix

--- Nix/modules/packages/neovim.nix
+++ Nix/modules/packages/neovim.nix
@@ -1,36 +1,30 @@
 { pkgs, ... }:
 
-let
-  config = pkgs.neovimUtils.makeNeovimConfig {
+{
+  programs.neovim = {
+    enable = true;
     defaultEditor = true;
     withNodeJs = true;
-    extraLuaPackages = p: [ p.magick ];
-    extraPackages = p: [
-      p.imagemagick
-      p.lua5_1
-      p.luarocks
-      p.imagemagick
-      p.stylua
-      p.readline
-      p.pkg-config
-      p.gcc
-      p.cargo
-      p.ripgrep
-      p.fzf
-      p.fd
-    ];
   };
-in {
-  nixpkgs.overlays = [
-    (_: super: {
-      neovim-custom = pkgs.wrapNeovimUnstable
-        (super.neovim-unwrapped.overrideAttrs
-          (oldAttrs: { buildInputs = oldAttrs.buildInputs; })) config;
-    })
+
+  environment.systemPackages = with pkgs; [
+    lua5_1
+    imagemagick
+    readline
+    pkg-config
+    gcc
+    cargo
+    ripgrep
+    fzf
+    fd
   ];
-  environment.systemPackages = with pkgs; [ neovim-custom ];
+
   environment.sessionVariables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
   };
+
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [ stylua ];
 }
+
