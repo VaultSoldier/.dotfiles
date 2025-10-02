3fb99ac91036a741c06a82e0ffd3b55ddffa9349
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-10-02 11:01:21 +0500

    VirtViewer

--- Nix/modules/packages/virt-manager.nix
+++ Nix/modules/packages/virt-manager.nix
@@ -1,6 +1,8 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.virt-manager.enable = true;
   users.groups.libvirtd.members = [ "vs" ];
   virtualisation.libvirtd.enable = true;
   virtualisation.spiceUSBRedirection.enable = true;
+
+  environment.systemPackages = with pkgs; [ virt-viewer ];
 }
