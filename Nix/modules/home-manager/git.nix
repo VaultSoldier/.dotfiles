46398cd9ceebcf3671cfb466392efa3cb930df8c
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-10-20 10:25:25 +0500

    NIXOS: Update git configuration new config version

--- Nix/modules/home-manager/git.nix
+++ Nix/modules/home-manager/git.nix
@@ -1,15 +1,15 @@
 { ... }: {
   programs.git = {
     enable = true;
-    userName = "VaultSoldier";
-    userEmail = "61462705+VaultSoldier@users.noreply.github.com";
-    extraConfig = {
+    settings = {
+      user.name = "VaultSoldier";
+      user.email = "61462705+VaultSoldier@users.noreply.github.com";
+      user.signingkey = "~/.ssh/ssh_keys/git_id_ed25519";
       init.defaultBranch = "dev";
       pull.rebase = true;
       commit.gpgsign = true;
       gpg.format = "ssh";
       gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-      user.signingkey = "~/.ssh/ssh_keys/git_id_ed25519";
     };
   };
 }
