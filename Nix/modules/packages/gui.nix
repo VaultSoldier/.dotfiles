2a30b8d34006db50885b4ed1c72415c9a94e0735
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-10-26 01:38:51 +0500

    NIXOS: remove WinBox4 Wayland fix, replace package with programs.winbox

--- Nix/modules/packages/gui.nix
+++ Nix/modules/packages/gui.nix
@@ -1,22 +1,12 @@
 { pkgs, ... }:
 
-let
-  # Wrap WinBox4 to force use of the XCB platform plugin under XWayland
-  winbox4Wrapped = pkgs.winbox4.overrideAttrs (drv: rec {
-    buildInputs = drv.buildInputs ++ [ pkgs.makeWrapper ];
-    postFixup = ''
-      wrapProgram $out/bin/WinBox \
-        --set QT_QPA_PLATFORM xcb
-    '';
-  });
-in {
+{
   environment.systemPackages = with pkgs; [
     # TODO: Temporary fix. Remove later
     (import (fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/4729404.tar.gz";
       sha256 = "08iy97dmc7d66g9fd31lch0qkl9j29apzhcm8qfl43h7q8nx9p9r";
     }) { system = pkgs.system; }).nextcloud-client
-    winbox4Wrapped
     kitty
     mpv
     kdePackages.gwenview
@@ -34,4 +24,9 @@ in {
   ];
 
   programs.amnezia-vpn.enable = true;
+  programs.winbox = {
+    enable = true;
+    package = pkgs.winbox4;
+    openFirewall = true;
+  };
 }
