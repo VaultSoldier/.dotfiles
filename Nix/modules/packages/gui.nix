a28543a5c092c33bac1827bdb247c32be6efd355
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-10-26 00:49:34 +0500

    NIXOS: nextcloud-client temporary fix

--- Nix/modules/packages/gui.nix
+++ Nix/modules/packages/gui.nix
@@ -11,9 +11,14 @@ let
   });
 in {
   environment.systemPackages = with pkgs; [
+    # TODO: Temporary fix. Remove later
+    (import (fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/4729404.tar.gz";
+      sha256 = "08iy97dmc7d66g9fd31lch0qkl9j29apzhcm8qfl43h7q8nx9p9r";
+    }) { system = pkgs.system; }).nextcloud-client
+    winbox4Wrapped
     kitty
     mpv
-    winbox4Wrapped
     kdePackages.gwenview
     bitwarden-desktop
     filezilla
@@ -23,7 +28,6 @@ in {
     chromium
     telegram-desktop
     nextcloud-talk-desktop
-    nextcloud-client
     onlyoffice-desktopeditors
     rustdesk-flutter
     obsidian
