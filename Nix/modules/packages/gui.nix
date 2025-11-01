5b22690a668ce291d82f097384215374c20b8300
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-11-02 02:56:31 +0500

    NIXOS: Remove version pin for nextcloud-client

--- Nix/modules/packages/gui.nix
+++ Nix/modules/packages/gui.nix
@@ -1,10 +1,6 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
-    # TODO: Temporary fix. Remove later
-    (import (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/4729404.tar.gz";
-      sha256 = "08iy97dmc7d66g9fd31lch0qkl9j29apzhcm8qfl43h7q8nx9p9r";
-    }) { system = pkgs.system; }).nextcloud-client
+    nextcloud-client
     kitty
     mpv
     kdePackages.gwenview
