c291c4ebee324a2c295764eec9f8905df68b760a
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-11-02 02:54:12 +0500

    NIXOS: Update zapret params

--- Nix/modules/system/zapret.nix
+++ Nix/modules/system/zapret.nix
@@ -3,11 +3,13 @@
     enable = true;
     udpSupport = true;
     udpPorts = [ "50000:50099" ];
+    httpMode = "full";
     params = [
       "--dpi-desync-any-protocol=1" # udp support
       "--filter-l7=discord,stun --dpi-desync=fake --new" # For Discord voice chats
+      "--dpi-desync-split-pos=2"
       "--dpi-desync=disorder"
-      "--dpi-desync-ttl=1"
+      "--dpi-desync-ttl=2"
       "--dpi-desync-autottl=2"
       "--dpi-desync-repeats=2"
     ];
