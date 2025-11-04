8d8117f717c7bc89425e59436fbab49a1683beba
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-11-05 00:14:39 +0500

    NIXOS: Add WARN comments to configuration.nix

--- Nix/hosts/laptop/configuration.nix
+++ Nix/hosts/laptop/configuration.nix
@@ -41,5 +41,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # WARN: DO NOT CHANGE!!!
   system.stateVersion = "25.05"; # Did you read the comment?
 }
