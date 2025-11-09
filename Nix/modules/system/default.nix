b41b14657facf6b6d971d1e703e3e83b14aa92d3
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-11-10 04:39:09 +0500

    NIXOS: Make zapret option

--- Nix/modules/system/default.nix
+++ Nix/modules/system/default.nix
@@ -1,11 +1,14 @@
-{ ... }: {
+{ lib, ... }: {
   imports = [
     ./audio.nix
     ./locale.nix
-    ./networking.nix
+    ./network.nix
     ./sddm.nix
     ./security.nix
     ./flatpak.nix
     ./zapret.nix
   ];
+
+  system.zapret.enable = lib.mkDefault false;
+  system.flatpak.enable = lib.mkDefault false;
 }
--- Nix/modules/system/zapret.nix
+++ Nix/modules/system/zapret.nix
@@ -1,17 +1,21 @@
-{ ... }: {
-  services.zapret = {
-    enable = true;
-    udpSupport = true;
-    udpPorts = [ "50000:50099" ];
-    httpMode = "full";
-    params = [
-      "--dpi-desync-any-protocol=1" # udp support
-      "--filter-l7=discord,stun --dpi-desync=fake --new" # For Discord voice chats
-      "--dpi-desync-split-pos=2"
-      "--dpi-desync=disorder"
-      "--dpi-desync-ttl=2"
-      "--dpi-desync-autottl=2"
-      "--dpi-desync-repeats=2"
-    ];
+{ lib, config, ... }: {
+  options.system = { zapret.enable = lib.mkEnableOption "Enable zapret"; };
+
+  config = lib.mkIf config.system.zapret.enable {
+    services.zapret = {
+      enable = true;
+      udpSupport = true;
+      udpPorts = [ "50000:50099" ];
+      httpMode = "full";
+      params = [
+        "--dpi-desync-any-protocol=1" # udp support
+        "--filter-l7=discord,stun --dpi-desync=fake --new" # For Discord voice chats
+        "--dpi-desync-split-pos=2"
+        "--dpi-desync=disorder"
+        "--dpi-desync-ttl=2"
+        "--dpi-desync-autottl=2"
+        "--dpi-desync-repeats=2"
+      ];
+    };
   };
 }
