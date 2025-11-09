7db069db3daa83a2022aab36378c1ca6e1f6504b
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-11-10 04:42:55 +0500

    NIXOS: hm stylix stylix.enable = true; line

--- Nix/modules/stylix/hm-stylix.nix
+++ Nix/modules/stylix/hm-stylix.nix
@@ -1,9 +1,9 @@
 { ... }: {
   imports = [ ./general-stylix.nix ];
 
+  stylix.enable = true;
   stylix.targets.btop.enable = false;
   stylix.targets.kitty.enable = false;
   stylix.targets.zen-browser.enable = false;
   stylix.targets.vesktop.enable = false;
-  stylix.targets.zen-browser.profileNames = [ "default" ];
 }
