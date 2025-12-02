8d66dd21a1f15f52940ed7aa0be1f1fcd9530ae4
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-12-02 14:26:13 +0500

    NIXOS: set use-xdg-base-directories = true
    Nix will conform to the XDG Base Directory Specification for files in $HOME.

--- Nix/configuration.nix
+++ Nix/configuration.nix
@@ -4,6 +4,7 @@
   nixpkgs.config.allowUnfree = true;
   hardware.enableAllFirmware = true;
   hardware.enableAllHardware = true;
+  nix.settings.use-xdg-base-directories = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   users.users.vs = {
