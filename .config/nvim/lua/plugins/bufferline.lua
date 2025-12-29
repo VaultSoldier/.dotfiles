8da12f8460f2a63d5d6bae8a919aebe28b4a26a0
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2025-12-29 07:18:22 +0500

    NVIM: bufferline change appearance, ignore filetype qf(quickfix list), fix offset

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -10,11 +10,28 @@ return {
         numbers = 'ordinal', -- "none" | "ordinal" | "buffer_id"
         close_command = 'bdelete! %d',
         offsets = {
-          { filetype = 'NvimTree', text = 'File Explorer', highlight = 'Directory' },
+          { filetype = 'neo-tree', text = 'File Explorer', highlight = 'Directory', separator = true },
         },
+        sort_by = 'insert_after_current',
         separator_style = 'thin', -- "slant" | "thick" | "thin" | { 'any', 'any' }
+        tab_size = 10,
         show_close_icon = false,
-        color_icons = true,
+        show_buffer_close_icons = false,
+        color_icons = false,
+
+        diagnostics = 'nvim_lsp',
+        diagnostics_update_on_event = true, -- use nvim's diagnostic handler
+        -- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local icon = level:match 'error' and ' ' or ' '
+          return ' ' .. icon .. count
+        end,
+        custom_filter = function(buf_number, buf_numbers)
+          -- filter out filetypes you don't want to see
+          if vim.bo[buf_number].filetype ~= 'qf' then
+            return true
+          end
+        end,
       },
     }
   end,
