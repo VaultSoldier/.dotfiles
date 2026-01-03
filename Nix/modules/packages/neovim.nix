b73a4023f576027f2deba0d4f27d7ae7175eca92
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-01-03 20:34:45 +0500

    NIXOS: minor refactor

--- Nix/modules/packages/neovim.nix
+++ Nix/modules/packages/neovim.nix
@@ -25,6 +25,8 @@
     VISUAL = "nvim";
   };
 
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [ stylua ];
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [ stylua ];
+  };
 }
