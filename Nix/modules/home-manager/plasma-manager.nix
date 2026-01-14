a7c75f259672698c1da79ca78a5f2586e1d660f3
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-01-15 00:53:50 +0500

    NIXOS: update plasma-manager hotkeys

--- Nix/modules/home-manager/plasma-manager.nix
+++ Nix/modules/home-manager/plasma-manager.nix
@@ -1,4 +1,5 @@
-{ inputs, ... }: {
+{ inputs, ... }:
+{
   imports = [ inputs.plasma-manager.homeModules.plasma-manager ];
 
   programs.plasma = {
@@ -16,20 +17,39 @@
       Shortcuts.SaveAs = "Ctrl+Alt+S";
     };
     shortcuts = {
-      kwin."Window Close" = [ "Meta+Q" "Alt+F4" ];
-      mediacontrol.previousmedia = [ "Meta+F2" "Media Previous" ];
-      mediacontrol.nextmedia = [ "Meta+F3" "Media Next" ];
-      mediacontrol.playpausemedia = [ "Media Play" "Meta+F1" ];
+      "KDE Keyboard Layout Switcher"."Switch to Next Keyboard Layout" = "Alt+Shift";
+      kwin."Edit Tiles" = [ ];
+      kwin."Window Close" = [
+        "Alt+F4"
+        "Meta+Q"
+      ];
+      mediacontrol.previousmedia = [
+        "Meta+F2"
+        "Media Previous"
+      ];
+      mediacontrol.nextmedia = [
+        "Meta+F3"
+        "Media Next"
+      ];
+      mediacontrol.playpausemedia = [
+        "Media Play"
+        "Meta+F1"
+      ];
       mediacontrol.mediavolumedown = [ ];
       mediacontrol.mediavolumeup = [ ];
 
       plasmashell."manage activities" = "";
       "services/kitty.desktop"._launch = "Meta+T";
       "services/org.kde.konsole.desktop"._launch = [ ];
-      "services/org.kde.spectacle.desktop".RectangularRegionScreenShot =
-        [ "Meta+Shift+Print" "Ctrl+Shift+S" ];
+      "services/org.kde.spectacle.desktop".RectangularRegionScreenShot = [
+        "Meta+Shift+Print"
+        "Ctrl+Shift+S"
+      ];
       "services/org.kde.spectacle.desktop"._launch = "Print";
-      "services/systemsettings.desktop"._launch = [ "Meta+I" "Tools" ];
+      "services/systemsettings.desktop"._launch = [
+        "Meta+I"
+        "Tools"
+      ];
     };
   };
 }
