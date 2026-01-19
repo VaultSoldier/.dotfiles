96994681c9e47d80922c09edc84ae80ab9e8e4cc
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-01-19 15:38:21 +0500

    NIXOS: rm fix QT_QML_IMPORT_PATH

--- Nix/hosts/desktop/caelestia.nix
+++ Nix/hosts/desktop/caelestia.nix
@@ -1,17 +1,4 @@
 { inputs, pkgs, ... }:
-let # Fixes launching with Kde Plasma installed
-  kirigami = pkgs.kdePackages.kirigami.unwrapped;
-  wrappedCaelestia =
-    inputs.caelestia-shell.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs
-      (old: {
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.makeWrapper ];
-        postFixup = (old.postFixup or "") + ''
-          wrapProgram $out/bin/caelestia-shell \
-            --set QT_QML_IMPORT_PATH "${kirigami}/lib/qt-6/qml" \
-            --set QML2_IMPORT_PATH ""
-        '';
-      });
-in
 {
   imports = [ inputs.caelestia-shell.homeManagerModules.default ];
 
@@ -34,7 +21,6 @@ in
   programs.caelestia = {
     enable = true;
     systemd.enable = false;
-    package = wrappedCaelestia;
 
     settings = {
       appearance = {
