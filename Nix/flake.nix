dafdce03bbde4d188f9509d51bb4c8ac9af22f4b
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-02-06 04:06:15 +0500

    NIXOS: rm affinity-nix pckg from #bronze

--- Nix/flake.nix
+++ Nix/flake.nix
@@ -158,8 +158,6 @@
             home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.backupFileExtension = "bckp";
             home-manager.users.vs.imports = [ ./modules/home-manager ];
-
-            environment.systemPackages = [ affinity-nix.packages.x86_64-linux.v3 ];
           }
         ];
       };
