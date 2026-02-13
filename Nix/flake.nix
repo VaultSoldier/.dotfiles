ef54d95e9ca4ee0532e414c1ffb3cae7e046af98
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-02-13 16:16:16 +0500

    NIXOS: refactor flake.nix and home-manager

--- Nix/flake.nix
+++ Nix/flake.nix
@@ -7,33 +7,27 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake/beta";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     stylix = {
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     plasma-manager = {
       url = "github:nix-community/plasma-manager";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
-
     caelestia-shell = {
       url = "github:VaultSoldier/shell?ref=NightLight";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     mikuboot = {
       url = "gitlab:evysgarden/mikuboot";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -50,116 +44,106 @@
       stylix,
       spicetify-nix,
       zen-browser,
-      affinity-nix,
       caelestia-shell,
       mikuboot,
       ...
     }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      commonModules = [
+        ./configuration.nix
+        ./modules/desktop-environments
+        ./modules/packages/games
+        ./modules/packages
+        ./modules/stylix
+        ./modules/system
+        mikuboot.nixosModules.default
+        home-manager.nixosModules.home-manager
+        sops-nix.nixosModules.sops
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.backupFileExtension = "bckp";
+          home-manager.users.vs.imports = [ ./modules/home-manager ];
+        }
+      ];
     in
     {
       nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = inputs;
-
         modules = [
-          ./configuration.nix
+          {
+            desktop = {
+              plasma6.enable = true;
+              hyprland.enable = true;
+              caelestia.enable = true;
+            };
+            games = {
+              lutris.enable = false;
+            };
+            system = {
+              flatpak.enable = true;
+            };
+
+            home-manager.users.vs.imports = [ ./hosts/desktop/home-manager.nix ];
+          }
           ./hosts/desktop
-          ./modules/system
-          ./modules/packages
-          ./modules/packages/games
           ./modules/hardware/nvidia.nix
           ./modules/hardware/fifine-am8.nix
-          ./modules/desktop-environments
-          ./modules/stylix
-          mikuboot.nixosModules.default
-          home-manager.nixosModules.home-manager
-          sops-nix.nixosModules.sops
-
-          {
-            desktop.plasma6.enable = true;
-            desktop.hyprland.enable = true;
-            desktop.caelestia.enable = true;
-            system.flatpak.enable = true;
-            games.lutris.enable = false;
-
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-            home-manager.backupFileExtension = "bckp";
-            home-manager.users.vs.imports = [ ./modules/home-manager ];
-          }
-        ];
+        ]
+        ++ commonModules;
       };
 
       nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = inputs;
-
         modules = [
-          ./configuration.nix
-          ./hosts/laptop
-          ./modules/system
-          ./modules/packages
-          ./modules/packages/games
-          ./modules/desktop-environments
-          ./modules/stylix
-          mikuboot.nixosModules.default
-          home-manager.nixosModules.home-manager
-          sops-nix.nixosModules.sops
-
           {
-            desktop.plasma6.enable = true;
-            desktop.hyprland.enable = true;
-            desktop.caelestia.enable = true;
-            system.flatpak.enable = true;
+            desktop = {
+              plasma6.enable = true;
+              hyprland.enable = true;
+              caelestia.enable = true;
+            };
+            games = {
+              osu-lazer.enable = false;
+              lutris.enable = false;
+            };
+            system = {
+              flatpak.enable = true;
+            };
 
-            games.osu-lazer.enable = false;
-            games.lutris.enable = false;
-
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-            home-manager.backupFileExtension = "bckp";
-            home-manager.users.vs.imports = [ ./modules/home-manager ];
+            home-manager.users.vs.imports = [ ./hosts/laptop/home-manager.nix ];
           }
-        ];
+          ./hosts/laptop
+        ]
+        ++ commonModules;
       };
 
       nixosConfigurations.bronze = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = inputs;
-
         modules = [
-          ./configuration.nix
-          ./hosts/bronze
-          ./modules/system
-          ./modules/packages
-          ./modules/packages/games
-          ./modules/desktop-environments
-          ./modules/stylix
-          mikuboot.nixosModules.default
-          home-manager.nixosModules.home-manager
-          sops-nix.nixosModules.sops
-
           {
-            desktop.plasma6.enable = false;
-            desktop.hyprland.enable = true;
-            desktop.caelestia.enable = true;
-            system.flatpak.enable = true;
+            desktop = {
+              plasma6.enable = false;
+              hyprland.enable = true;
+              caelestia.enable = true;
+            };
+            games = {
+              osu-lazer.enable = false;
+              lutris.enable = false;
+            };
+            system = {
+              flatpak.enable = true;
+            };
 
-            games.osu-lazer.enable = false;
-            games.lutris.enable = false;
-
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-            home-manager.backupFileExtension = "bckp";
-            home-manager.users.vs.imports = [ ./modules/home-manager ];
+            home-manager.users.vs.imports = [ ./hosts/bronze/home-manager.nix ];
           }
-        ];
+          ./hosts/bronze
+        ]
+        ++ commonModules;
       };
     };
 }
