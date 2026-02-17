363409a0894404492c42a249a36d54cac9a84d0a
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-02-17 17:45:58 +0500

    NIXOS: temp patch for nvidia-590 with kernel-6.19

--- Nix/modules/hardware/nvidia.nix
+++ Nix/modules/hardware/nvidia.nix
@@ -1,6 +1,5 @@
 {
   config,
-  lib,
   pkgs,
   ...
 }:
@@ -21,7 +20,23 @@
     powerManagement.finegrained = false;
     open = true;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package =
+      let
+        base = config.boot.kernelPackages.nvidiaPackages.beta;
+        cachyos-nvidia-patch = pkgs.fetchpatch {
+          url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
+          sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
+        };
+
+        # Patch the appropriate driver based on config.hardware.nvidia.open
+        driverAttr = if config.hardware.nvidia.open then "open" else "bin";
+      in
+      base
+      // {
+        ${driverAttr} = base.${driverAttr}.overrideAttrs (oldAttrs: {
+          patches = (oldAttrs.patches or [ ]) ++ [ cachyos-nvidia-patch ];
+        });
+      };
   };
 
   nixpkgs.config.cudaSupport = true;
