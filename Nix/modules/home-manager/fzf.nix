88bff9c8e333beb3d4a907201010aab47290089c
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-03-02 13:00:28 +0500

    NIXOS: fzf new ignore paths

--- Nix/modules/home-manager/fzf.nix
+++ Nix/modules/home-manager/fzf.nix
@@ -3,7 +3,15 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
-    defaultCommand = ''fd --type f --exclude ".steam/steam/steamapps/compatdata" ".steam/steam/steamrt64/steam-runtime-steamrt"'';
-    fileWidgetCommand = ''fd --type f --exclude ".steam/steam/steamapps/compatdata" ".steam/steam/steamrt64/steam-runtime-steamrt"'';
+    defaultCommand = ''
+      fd --type f --exclude 
+          "$HOME/.wine/dosdevices/z" "$HOME/.wine/drive_c"
+          "$HOME/.steam/steam/steamapps/compatdata" "$HOME/.steam/steam/steamrt64/steam-runtime-steamrt"
+    '';
+    fileWidgetCommand = ''
+      fd --type f --exclude 
+          "$HOME/.wine/dosdevices/z" "$HOME/.wine/drive_c"
+          "$HOME/.steam/steam/steamapps/compatdata" "$HOME/.steam/steam/steamrt64/steam-runtime-steamrt"
+    '';
   };
 }
