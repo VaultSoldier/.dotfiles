3aa220746c847c9586b78ebcc8666c40f9331c06
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-03-07 19:35:24 +0500

    NIXOS: change ssh keys path

--- Nix/modules/home-manager/git.nix
+++ Nix/modules/home-manager/git.nix
@@ -4,7 +4,7 @@
     settings = {
       user.name = "VaultSoldier";
       user.email = "61462705+VaultSoldier@users.noreply.github.com";
-      user.signingkey = "~/.ssh/ssh_keys/git_id_ed25519";
+      user.signingkey = "git_id_ed25519";
       init.defaultBranch = "dev";
       pull.rebase = true;
       commit.gpgsign = true;
