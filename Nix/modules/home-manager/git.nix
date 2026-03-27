b5a4cf98a9a152a0b7a2e4cdc499a8f70181c49e
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-03-27 22:28:44 +0500

    nixos: different git commit signing for selfhost gitlab

--- Nix/modules/home-manager/git.nix
+++ Nix/modules/home-manager/git.nix
@@ -11,5 +11,16 @@
       gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
       gpg.format = "ssh";
     };
+
+    includes = [
+      {
+        condition = "hasconfig:remote.*.url:ssh://git@gitlab.ssk-yamal.ru:*/**";
+        contents = {
+          user.name = "agent_smith";
+          user.email = "35-agent_smith@users.noreply.gitlab.ssk-yamal.ru";
+          user.signingkey = "~/.ssh/pubkeys/work/work_id_ed25519.pub";
+        };
+      }
+    ];
   };
 }
