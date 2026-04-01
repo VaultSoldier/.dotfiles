167b6f8949a03d5ca3d712604382fc5c6d4b933a
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-04-01 09:10:29 +0500

    nixos(hosts): rm #work

--- Nix/hosts/work/syncthing.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-{
-  imports = [ ../../modules/packages/syncthing.nix ];
-
-  services.syncthing = {
-    key = "/run/secrets/syncthing_work_key";
-    cert = "/run/secrets/syncthing_work_cert";
-  };
-}
