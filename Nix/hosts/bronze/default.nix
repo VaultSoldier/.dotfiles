c87b21b732a94391e2a121f6e97ef9256383894a
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-04-01 09:13:39 +0500

    nixos(hosts): rm fprintd on #bronze

--- Nix/hosts/bronze/default.nix
+++ Nix/hosts/bronze/default.nix
@@ -4,7 +4,6 @@
     ./hardware-configuration.nix
     ./configuration.nix
     ./power-management.nix
-    ./fprintd.nix
   ];
 
   options.desktop.caelestia.enable = lib.mkEnableOption "Enable caelestia";
--- Nix/hosts/bronze/fprintd.nix
+++
@@ -1,5 +0,0 @@
-{ config, lib, pkgs, ... }: {
-  services.fprintd.enable = true;
-  security.pam.services.sudo-rs.fprintAuth = true;
-  security.pam.services.sddm.fprintAuth = true;
-}
