be75f84d4a33f392b220f6c2062178a5224003f7
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-04-05 23:57:04 +0500

    hypr(scripts): simpler submap toggling

--- .config/hypr/scripts/submap-daemon.sh
+++ .config/hypr/scripts/submap-daemon.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 socket="$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock"
 
-while true; do
-    socat -U - UNIX-CONNECT:"$socket" | while read -r line; do
-        if [[ "$line" == "activespecial>>"* ]]; then
-            name="${line#activespecial>>}"
-            name="${name%%,*}"
-            if [[ -z "$name" ]]; then
-                layout=$(hyprctl activeworkspace -j | jq -r '.tiledLayout')
-                if [[ "$layout" == "scrolling" ]]; then
-                    hyprctl dispatch submap scrolling
-                else
-                    hyprctl dispatch submap global
-                fi
-            fi
+handle() {
+    local line="$1"
+
+    [[ "$line" != "activespecial>>"* ]] && return
+
+    name="${line#activespecial>>}"
+    name="${name%%,*}"
+
+    # if special workspace set submap global
+    if [[ -n "$name" ]]; then
+        hyprctl dispatch submap global
+    else
+        # if no special workspace set based on current layout
+        layout=$(hyprctl activeworkspace -j | jq -r '.tiledLayout')
+        if [[ "$layout" == "scrolling" ]]; then
+            hyprctl dispatch submap scrolling
+        else
+            hyprctl dispatch submap global
         fi
-    done
-    sleep 1  # wait before reconnect attempt
-done
+    fi
+}
+
+socat -U - UNIX-CONNECT:"$socket" | while read -r line; do handle "$line"; done
