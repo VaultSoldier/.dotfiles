37e57bfef205caac7023859f86b3e679909e6bbd
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-04-07 13:04:45 +0500

    hypr(scripts): handle submap switching when monitor focus changes

--- .config/hypr/scripts/submap-daemon.sh
+++ .config/hypr/scripts/submap-daemon.sh
@@ -1,26 +1,38 @@
 #!/usr/bin/env bash
 socket="$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock"
 
+set_layout_by_submap() {
+    local layout
+    layout=$(hyprctl activeworkspace -j | jq -r '.tiledLayout')
+    if [[ "$layout" == "scrolling" ]]; then
+        hyprctl dispatch submap scrolling
+    else
+        hyprctl dispatch submap global
+    fi
+
+}
+
 handle() {
     local line="$1"
 
-    [[ "$line" != "activespecial>>"* ]] && return
+    case "$line" in
+    "activespecial>>"*)
+        name="${line#activespecial>>}"
+        name="${name%%,*}"
 
-    name="${line#activespecial>>}"
-    name="${name%%,*}"
-
-    # if special workspace set submap global
-    if [[ -n "$name" ]]; then
-        hyprctl dispatch submap global
-    else
-        # if no special workspace set based on current layout
-        layout=$(hyprctl activeworkspace -j | jq -r '.tiledLayout')
-        if [[ "$layout" == "scrolling" ]]; then
-            hyprctl dispatch submap scrolling
-        else
+        if [[ -n "$name" ]]; then
             hyprctl dispatch submap global
+        else
+            set_layout_by_submap
         fi
-    fi
+        ;;
+
+    "focusedmonv2>>"*)
+        special=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .specialWorkspace.name')
+        [[ -n "$special" ]] && return
+        set_layout_by_submap
+        ;;
+    esac
 }
 
 socat -U - UNIX-CONNECT:"$socket" | while read -r line; do handle "$line"; done
