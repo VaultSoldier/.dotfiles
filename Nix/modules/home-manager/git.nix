97ef84246df35995297bb41b644aae7267bf4126
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-04-08 21:01:40 +0500

    nixos: change user.signingkey location

--- Nix/modules/home-manager/git.nix
+++ Nix/modules/home-manager/git.nix
@@ -18,7 +18,7 @@
         contents = {
           user.name = "agent_smith";
           user.email = "35-agent_smith@users.noreply.gitlab.ssk-yamal.ru";
-          user.signingkey = "~/.ssh/pubkeys/work/work_id_ed25519.pub";
+          user.signingkey = "~/.ssh/pubkeys-work/work_id_ed25519.pub";
         };
       }
     ];
