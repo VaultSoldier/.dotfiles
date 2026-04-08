e5ebcaf561e90c966ca1bce9ceb0fa42e4cc9ec2
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-04-08 20:53:54 +0500

    nixos: change user.signingkey location

--- Nix/modules/home-manager/git.nix
+++ Nix/modules/home-manager/git.nix
@@ -7,7 +7,7 @@
       init.defaultBranch = "dev";
       user.name = "VaultSoldier";
       user.email = "61462705+VaultSoldier@users.noreply.github.com";
-      user.signingkey = "~/.ssh/git_id_ed25519.pub";
+      user.signingkey = "~/.ssh/pubkeys/git_id_ed25519.pub";
       gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
       gpg.format = "ssh";
     };
