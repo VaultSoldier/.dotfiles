58253e2a8f21217d7a724cf670d395f96f7a0450
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-04-21 08:50:35 +0500

    nixos(hosts): rm system76-scheduler

--- Nix/hosts/laptop/power-management.nix
+++ Nix/hosts/laptop/power-management.nix
@@ -2,6 +2,5 @@
 
 {
   services.upower.enable = true;
-  services.system76-scheduler.settings.cfsProfiles.enable = true; # Better scheduling for CPU cycles - thanks System76!!!
   services.power-profiles-daemon.enable = true;
 }
