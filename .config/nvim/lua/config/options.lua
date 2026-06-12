cd7c472c9dfe17fca0c00b6ae115f8fe62ca72e0
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-06-13 00:35:41 +0500

    feat(nixos): tabstop 2 -> 4

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -17,9 +17,9 @@ o.number = true
 o.relativenumber = true
 
 -- Indenting
-o.tabstop = 2
-o.softtabstop = 2
-o.shiftwidth = 2
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
 o.expandtab = true
 o.smartindent = true
 
