5db68de9baca172d357bcb4dbd4718d927063fef
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-06-15 06:19:01 +0500

    feat(hypr): integration with awww

--- .config/hypr/configs/autostart.lua
+++ .config/hypr/configs/autostart.lua
@@ -3,11 +3,12 @@ local scripts = home .. "/.config/hypr/scripts/"
 
 hl.on("hyprland.start", function()
 	hl.exec_cmd("mpris-proxy") -- Forward bluetooth media commands to MPRIS
+	hl.exec_cmd("awww-daemon")
+	hl.exec_cmd(scripts .. "awww_randomize.sh ".. home .. "/Pictures/Wallpapers " .. "7200")
 	hl.exec_cmd("caelestia resizer -d")
 	hl.exec_cmd("QT_QPA_PLATFORMTHEME=kde caelestia shell -d")
 	hl.exec_cmd("wl-paste --type text --watch cliphist store")
 	hl.exec_cmd("wl-paste --type image --watch cliphist store")
-	hl.exec_cmd(scripts .. "random_wallpaper.sh")
 	hl.exec_cmd("bitwarden")
 	hl.exec_cmd("trash-empty 30") -- Auto delete trash 30 days old
 end)
