e200142ee135f286302d421efd52a08fdd3c3f3f
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-07-24 16:47:36 +0500

    feat(nvim): nvim-flutter rm lsp.color

--- .config/nvim/lua/plugins/flutter-tools.lua
+++ .config/nvim/lua/plugins/flutter-tools.lua
@@ -5,18 +5,6 @@ return {
     'nvim-lua/plenary.nvim',
   },
   config = function()
-    require('flutter-tools').setup {
-      lsp = {
-        color = { -- show the derived colours for dart variables
-          enabled = true, -- whether or not to highlight color variables
-          background = true, -- highlight the background
-          ---@diagnostic disable-next-line: assign-type-mismatch
-          background_color = { r = 19, g = 17, b = 24 }, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
-          foreground = false, -- highlight the foreground
-          virtual_text = true, -- show the highlight using virtual text
-          virtual_text_str = '■', -- the virtual text character to highlight
-        },
-      },
-    }
+    require('flutter-tools').setup {}
   end,
 }
