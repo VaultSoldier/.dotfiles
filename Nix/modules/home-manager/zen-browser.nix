e0786d9521b265ad5ce6d31a32df32ae6552a864
VaultSoldier <61462705+VaultSoldier@users.noreply.github.com>
2026-08-25 15:24:58 +0500

    chore(hypr): update zen-browser keyboard ver

--- Nix/modules/home-manager/zen-browser.nix
+++ Nix/modules/home-manager/zen-browser.nix
@@ -66,7 +66,7 @@
 
     # WARN: Fails activation on schema changes to detect potential regressions
     # Find this in about:config or prefs.js of your profile
-    keyboardShortcutsVersion = 19;
+    keyboardShortcutsVersion = 20;
 
     # INFO: Finding Shortcut IDs
     # jq -c '.shortcuts[] | {id, key, keycode, action}' ~/.config/zen/default/zen-keyboard-shortcuts.json | fzf
